48f2797569a2df5a1853b22972e9437c6d8516d2
CSLeiYang <csleiyang@gmail.com>
2024-10-07 19:13:35 +0800

    add local

--- web/pub.js
+++ web/pub.js
@@ -109,6 +109,12 @@ async function updateLocalStream() {
             }
         });
 
+        // 更新本地视频元素
+        const localVideo = document.getElementById('local-video');
+        if (localVideo) {
+            localVideo.srcObject = localStream;
+        }
+
     } catch (error) {
         displayMessage(`initLocalStream error: ${error.message}`, true); // 使用新的函数名并标记为错误
     }
@@ -216,9 +222,8 @@ async function joinSession(confName) {
         document.getElementById('remote-videos').appendChild(el);
     };
 
-    const localVideo = document.getElementById('video');
+    const localVideo = document.getElementById('local-video');
     localVideo.srcObject = localStream;
-
 
     ws.onmessage = async (event) => {
         const jsonObject = JSON.parse(event.data);
